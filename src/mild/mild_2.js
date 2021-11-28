f3bbe348721f6672f44c8f4fdba59346fd3a4d7c
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-28 18:16:32 -0500

    mild_1

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -69,13 +69,15 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let objectout = {};
-   for(let i = 0; i < object.length; i++) {
-      var a = object.keyList[i];
-      if(a != key) {
-         objectout[a] = object[a];
-      }
-   }
+   // let objectout = {};
+   // for(let i = 0; i < object.length; i++) {
+   //    var a = object.keyList[i];
+   //    if(a != key) {
+   //       objectout[a] = object[a];
+   //    }
+   // }
+   objectout = object;
+   removeKey(objectout,key);
    return objectout;
 
 }
