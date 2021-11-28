7bcfe39e09949dff3d37a37e3a25da8aa9d83581
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-27 22:06:28 -0500

    mild_1

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -7,7 +7,7 @@
  */
 export function identifyVariable(variable) {
    var type = console.log(typeof variable);
-   return "{ type: '"+type + "', value: "+variable;
+   return "{ type: '"+type + "', value: "+variable +' }';
 }
 
 
