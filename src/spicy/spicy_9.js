0af8f6bd87f7b38083732d7f821f2809815f56ba
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-28 21:48:41 -0500

    spicy-9

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -57,7 +57,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -65,7 +65,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tentimes(50);
 };
 
 
