4a02dcdbd7bcae38fd944582cc011796744277a3
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-28 21:42:47 -0500

    spicy-9

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -47,8 +47,8 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-    var num2 = window.prompt();
-    return mun1*num2;
+    var num2 = 2;
+    return num1*mun2;
 
 };
 
