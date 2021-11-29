d10620a9142693fa1d72d89568bf741ae43ecfa4
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-29 01:15:59 -0500

    spicy-9

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -20,15 +20,15 @@ see under the methods section
  * @param {allCarStats.ratioHybrids} ratio of cars that are hybrids
  */
 export const allCarStats = {
-    avgMpg: function(){
+    avgMpg: (city, highway) => {
         var avgcity = 0;
         var avghighway = 0;
         var len = mpg_data.length;
-        for(let i = 0; i < len; i++) {
-           avgcity += mpg_data[i].city;
-           avghighway += mpg_data[i].highway;
-    } 
-    return "{ city: "+avgcity/len+", highway: "+avghighway/len+" }"; 
+        for (let i = 0; i < len; i++) {
+            avgcity += mpg_data[i].city;
+            avghighway += mpg_data[i].highway;
+        }
+        return "{ city: " + avgcity / len + ", highway: " + avghighway / len + " }";
     } 
     ,
     allYearStats: undefined,
