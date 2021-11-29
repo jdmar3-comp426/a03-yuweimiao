6dbc3ed727a9231d1167abfae3196f7830db4996
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-28 20:47:19 -0500

    spicy-9

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -17,7 +17,7 @@
 export const repeat = (fn, n, ...params) => {
     const arrayout = [];
     for(let i = 0; i<n; i++) {
-        arrayout[0] += fn(params);
+        arrayout[i] += fn(params)[0];
     }
     return arrayout;
 };
